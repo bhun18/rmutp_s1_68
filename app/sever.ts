1d2fab658fc7ece3f1e669c7f6b8559578038f08
Thanusphol Kruthong <thanusphol-k@rmutp.ac.th>
2025-08-27 14:02:45 +0700

    Run

--- app/sever.ts
+++ app/sever.ts
@@ -1,6 +1,9 @@
 import { serve } from "@hono/node-server";
 import app from "./index";
 
+
+const port = Number(process.env.PORT ?? 3000)
+
 serve(app, (info) => {
-    console.log(`Server is running on ${info.port}`);
+    console.log("Running server on port", info.port);
 });
