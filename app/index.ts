6a924c81d8b151a1393b75a72ede1c5d723cba44
Thanusphol Kruthong <thanusphol-k@rmutp.ac.th>
2025-09-03 17:32:15 +0700

    update

--- app/index.ts
+++ app/index.ts
@@ -1,41 +1,50 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "../generated/prisma/client";
+//import PrismaClient from "@prisma/client";                                                                                         
+import { PrismaClient } from "@prisma/client";  
 import * as bcrypt from "bcrypt";
-// import { PrismaClient } from "../generated/prisma/client";
+
 
 const prisma = new PrismaClient();
-
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello, World!"));
+app.get("/", (c) => c.text("Hello World!"));
 
 app.get("/about", (c) => {
-    return c.json({
-        message: "Thanusphol Kruthong"
+    return c.json ({
+        mwssage: "Thanusphol Kruthong"
     });
 });
-app.get("/profile", async(c) => {
-    // logic
+app.get("/profile",async(c) => {
+    //logic
     const profile = await prisma.profile.findMany();
     return c.json(profile);
 });
-app.post("/profile", async(c) => {
 
+app.post("/profile",async(c) => {
+
+    //logic to create a new profile
     const body = await c.req.json();
-    console.log('input of profile ',body);
-    console.log('body.password(original)', body.password);
+    console.log('input of profile ', body);
+    console.log('body.password(original) ', body.password);
 
     //encode password
-    const passwordHash = await bcrypt.hash(body.password,10);
-    console.log('hash.password(after)', passwordHash);
+    const passwordHash = await bcrypt.hash(body.password, 10);
+    console.log('hash.password(after) ', passwordHash);
     body.password = passwordHash;
-    console.log('body.password(replace)', body);``
-    //save to d
+    console.log('body.password(replace) ', body);
+
+    //save to db
+        body.status = false;
+        const result = await prisma.profile.create({
+        data: body
+    });
+
     //output response
     return c.json({
-        message: "creat profile completed" 
+        message: "Create profile completed",
+        data: result
     });
 });
-
 
 export default app;
--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,42 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const hono_1 = require("hono");
+//import { PrismaClient } from "../generated/prisma/client";
+//import PrismaClient from "@prisma/client";                                                                                         
 const client_1 = require("@prisma/client");
 const bcrypt = require("bcrypt");
-// import { PrismaClient } from "../generated/prisma/client";
 const prisma = new client_1.PrismaClient();
 const app = new hono_1.Hono();
-app.get("/", (c) => c.text("Hello, World!"));
+app.get("/", (c) => c.text("Hello World!"));
 app.get("/about", (c) => {
     return c.json({
-        message: "Thanusphol Kruthong"
+        mwssage: "Thanusphol Kruthong"
     });
 });
 app.get("/profile", async (c) => {
-    // logic
+    //logic
     const profile = await prisma.profile.findMany();
     return c.json(profile);
 });
 app.post("/profile", async (c) => {
+    //logic to create a new profile
     const body = await c.req.json();
     console.log('input of profile ', body);
-    console.log('body.password(original)', body.password);
+    console.log('body.password(original) ', body.password);
     //encode password
-    const passwordHash = await bcrypt.hash(body.password, 10);
-    console.log('hash.password(after)', passwordHash);
+    const passwordHash = await bcrypt.hash(body.password, 18);
+    console.log('hash.password(after) ', passwordHash);
     body.password = passwordHash;
-    console.log('body.password(replace)', body);
-    ``;
-    //save to d
+    console.log('body.password(replace) ', body);
+    //save to db
+    body.status = false;
+    const result = await prisma.profile.create({
+        data: body
+    });
     //output response
     return c.json({
-        message: "creat profile completed"
+        message: "Create profile completed",
+        data: result
     });
 });
 exports.default = app;
